a20e70fd6b5cc62dc02b7eefa2998a373c0b0a3a
mateus-jaszumbek <mateusjaszumbek@hotmail.com>
2024-04-11 22:31:51 -0300

    Add project files.

---
+++ Biblioteca/Controllers/EmprestimoController.cs
@@ -0,0 +1,91 @@
+﻿using Biblioteca.Data;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Biblioteca.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private SistemaDbContext _db;
+
+        public EmprestimoController(SistemaDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModels> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimoModels emprestimos = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (id == null) { return NotFound(); }
+
+            return View(emprestimos);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+
+            EmprestimoModels emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null) { NotFound(); }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost("EditarEmprestimo/{id}")]
+        public IActionResult Cadastrar(EmprestimoModels emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(EmprestimoModels emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimoModels emprestimo)
+        {
+            if (emprestimo == null) {  return NotFound(); }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Biblioteca/Data/SistemaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
+{
+    public class SistemaDbContext : DbContext
+    {
+        public SistemaDbContext() { }
+        public SistemaDbContext(DbContextOptions<SistemaDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<EmprestimoModels> Emprestimos { get; set; }
+
+    }
+}
